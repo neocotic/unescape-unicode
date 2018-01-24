8e9fdae8421b2a9c969c6a6482dd6f6d5e0f8eb1
Alasdair Mercer <mercer.alasdair@gmail.com>
2018-01-24 17:13:41 +0000

    implemented initial logic and added unit tests

--- src/unescape-unicode.js
+++ src/unescape-unicode.js
@@ -24,15 +24,98 @@
 
 // TODO: Complete
 
+const errors = require('./errors');
+
+/**
+ * TODO: Document
+ *
+ * @param {string} input -
+ * @param {number} start -
+ * @return {number}
+ */
+function getOffset(input, start) {
+  let ch = input[start];
+
+  switch (ch) {
+  case '\\':
+    ch = input[start + 1];
+    if (ch !== 'u') {
+      throw new Error(errors.unexpectedCharacter(ch, start + 1));
+    }
+
+    return 2;
+  case 'u':
+    return 1;
+  default:
+    return 0;
+  }
+}
+
 /**
  * TODO: Document
  *
  * @param {?string} input -
  * @param {number} [start=0] -
  * @return {?string}
+ * @throws {Error}
  */
 function unescapeUnicode(input, start) {
-  // TODO: Complete
+  if (input == null) {
+    return input;
+  }
+  if (start == null || start < 0) {
+    start = 0;
+  }
+
+  start += getOffset(input, start);
+
+  const end = Math.min(start, input.length) + 4;
+  if (end > input.length) {
+    throw new Error(errors.insufficientCharacters(input.length - end));
+  }
+
+  let unicode = 0;
+
+  for (let i = start; i < end; i++) {
+    const ch = input[i];
+    const code = ch.charCodeAt(0);
+
+    // TODO: Switch to hash lookup to improve performance even further
+    switch (ch) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      unicode = (unicode << 4) + code - 0x30;
+      break;
+    case 'A':
+    case 'B':
+    case 'C':
+    case 'D':
+    case 'E':
+    case 'F':
+      unicode = (unicode << 4) + 10 + code - 0x41;
+      break;
+    case 'a':
+    case 'b':
+    case 'c':
+    case 'd':
+    case 'e':
+    case 'f':
+      unicode = (unicode << 4) + 10 + code - 0x61;
+      break;
+    default:
+      throw new Error(errors.unexpectedCharacter(ch, i));
+    }
+  }
+
+  return String.fromCharCode(unicode);
 }
 
 module.exports = unescapeUnicode;
